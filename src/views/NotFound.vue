5a4b04b39f275c00ea3b091607795147a690327e
muhammad <muhammad.yahya@microfocus.com>
2021-03-14 11:47:01 +0200

    Added work-flow integration to run tests on PR build

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -10,7 +10,7 @@
                         404 Not Found
                     </h2>
                     <div class="error-details">
-                        Sorry, an error has occured, Requested page not found!
+                        Sorry, an error has occurred, Requested page not found!
                     </div>
                     <div class="error-actions">
                         <router-link
